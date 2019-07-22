1182627aacfc70b485ada333826cfb6deca91537
root <pauloeduardo.sp@gmail.com>
2019-07-22 20:05:41 +0000

    commit remoto de script_git

--- Server/script_git.py
+++ Server/script_git.py
@@ -43,13 +43,20 @@ if commit is True:										#executa o commit caso haja alterações
 
 
 	for line in arquivo_copy:
-		os.system("cp " + line + " /home/github/" + line)
+		#os.system("cp " + line + " /home/github/Server" + line)
+		print('line = {}' .format(line))
+		cp_command = 'cp ' + line + ' /home/github/Server/' + line
+		print('cp_command = {}' .format(cp_command))
+		cp = os.popen(cp_command).read()
+		print("\n\n ########### cp \n")
+		print(cp)
+		print("\n ########### cp fim \n")
 
 	os.system("git -C \"/home/github\" add .")
 	os.system("git -C \"/home/github\" commit -a -m \"commit remoto de script_git\"")
 	os.system("git -C \"/home/github\" push -u origin master")
 	
-#teste envio arquiov 2
+#teste envio arquiov 5
 #git -C "/home/github" commit -a -m "commit remote"
 #	word = line.split()
 #	if len(word) > 1:
