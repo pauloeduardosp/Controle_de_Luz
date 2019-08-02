aa61f0c2a9b315b50c2e468606134b49a2eae490
Paulo VPS <pauloeduardo.sp@gmail.com>
2019-08-02 13:19:40 -0300

    commit remoto de script_git

--- Server/script_git.py
+++ Server/script_git.py
@@ -48,10 +48,9 @@ if commit is True:										#executa o commit caso haja alterações
 	for line in arquivo_copy:
 		os.system("cp /root/.node-red/" + line + " /home/github/Server/" + line)
 
-	exit()
+#	exit()
 	os.system("git -C \"/home/github\" add .")
 	os.system("git -C \"/home/github\" commit -a -m \"commit remoto de script_git\"")
 	os.system("git -C \"/home/github\" push  -u origin master")
 	
 	
-#tsete
